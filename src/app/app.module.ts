9addf078fb5055b7a9c50c5a41f05818b2ee877f
robbrownsell <robbrownsell@gmail.com>
2022-12-13 14:26:54 +0000

    added first component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,12 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {ProductListComponent} from "./products/product-list.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
+  declarations: [AppComponent, ProductListComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
